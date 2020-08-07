200b2dcce944596a7331512f5a7ead90b05c0f9b
renansdf <rnn.frts@gmail.com>
2020-08-07 19:52:46 -0300

    Setting up Netlify

--- src/components/Interface/index.tsx
+++ src/components/Interface/index.tsx
@@ -33,7 +33,7 @@ const Board: React.FC = () => {
   const getDecks = useCallback(async () => {
     const response: IAirTableResponseData = await api.get('', {
       headers: {
-        Authorization: 'Bearer keyNFJAPvRv0WaSBU',
+        Authorization: `Bearer ${process.env.AIRTABLE_TOKEN}`,
       }
     });
 
