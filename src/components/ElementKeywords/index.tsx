f8d63b8dcf31ebada90a795b40a6d2bc41d3ea11
renansdf <rnn.frts@gmail.com>
2020-10-19 15:44:16 -0300

    updated keywords

--- src/components/ElementKeywords/index.tsx
+++ src/components/ElementKeywords/index.tsx
@@ -9,34 +9,22 @@ const ElementKeywords: React.FC = () => {
         <div className="terra">
           <h3>Terra</h3>
           <section>
-            <h4>execução</h4>
-            <h4>concreto</h4>
-            <h4>físico</h4>
-            <h4>memória</h4>
-            <h4>tangível</h4>
+            <h4>ação</h4>
+            <h4>exemplo</h4>
           </section>
         </div>
         <div className="fogo">
           <h3>Fogo</h3>
           <section>
             <h4>propósito</h4>
-            <h4>inspiração</h4>
             <h4>essência</h4>
-            <h4>ideais</h4>
-            <h4>ideias</h4>
-            <h4>sentido</h4>
-            <h4>motivação</h4>
           </section>
         </div>
         <div className="agua">
           <h3>Água</h3>
           <section>
             <h4>método</h4>
-            <h4>fluxo</h4>
             <h4>processo</h4>
-            <h4>ir e vir</h4>
-            <h4>transformação</h4>
-            <h4>mudança</h4>
           </section>
         </div>
         <div className="ar">
@@ -44,8 +32,6 @@ const ElementKeywords: React.FC = () => {
           <section>
             <h4>relacionamento</h4>
             <h4>comunicação</h4>
-            <h4>dentro e fora</h4>
-            <h4>coletivo</h4>
           </section>
         </div>
       </PassoAPasso>
